33e7128f32ce8bf7a8bd68055cbef8b06d1580d5
oezert <oezert@tmbp.local>
2016-12-08 14:50:19 +0100

    add scaffold project

---
+++ src/CRT.java
@@ -0,0 +1,20 @@
+import java.math.BigInteger;
+
+/**
+ * Created by oezert on 08/12/2016.
+ */
+public class CRT {
+
+    BigInteger p;
+    BigInteger q;
+
+    public CRT(BigInteger p, BigInteger q) {
+        this.p = p;
+        this.q = q;
+    }
+
+    public BigInteger pow(BigInteger basis, BigInteger exponent) {
+        //TODO:
+        return null;
+    }
+}
---
+++ src/main.java
@@ -0,0 +1,8 @@
+/**
+ * Created by oezert on 08/12/2016.
+ */
+public class main {
+    public static void main(String[] args) {
+        //TODO:
+    }
+}
