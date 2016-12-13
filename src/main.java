58143904931a002eb2f8668dbc17f7036cf956bf
Adrian Frank <adri13@live.de>
2016-12-13 15:48:34 +0100

    implemented task 2

--- src/main.java
+++ src/main.java
@@ -4,9 +4,21 @@ import java.math.BigInteger;
  * Created by oezert on 08/12/2016.
  */
 public class main {
+	
+	static BigInteger p;
+	static BigInteger q;
+	static BigInteger n;
+	static BigInteger x;
+	static BigInteger y;
+	
     public static void main(String[] args) {
-        CRT crt = new CRT(BigInteger.valueOf(101), BigInteger.valueOf(103));
-        BigInteger result = crt.pow(BigInteger.valueOf(500), BigInteger.valueOf(3000));
-        System.out.println(result);
+    	p = BigInteger.valueOf(101);
+    	q = BigInteger.valueOf(103);
+    	n = p.multiply(q);
+    	x = BigInteger.valueOf(500);
+    	y = BigInteger.valueOf(3000);
+        CRT crt = new CRT(p, q);
+        System.out.println(crt.pow(x,y));
+        System.out.println(x.modPow(y, n));
     }
 }
