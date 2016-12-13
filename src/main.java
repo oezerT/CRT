fbdf49398b8c4cc3855eb049c5d44a06c5441c19
oezert <oezert@tmbp.local>
2016-12-13 16:42:45 +0100

    add function to create array of random BigIntegers in range 2 to maxValue

--- src/main.java
+++ src/main.java
@@ -1,4 +1,5 @@
 import java.math.BigInteger;
+import java.util.Random;
 
 /**
  * Created by oezert on 08/12/2016.
@@ -10,8 +11,9 @@ public class main {
 	static BigInteger n;
 	static BigInteger x;
 	static BigInteger y;
-	
-    public static void main(String[] args) {
+	static final BigInteger TWO = BigInteger.valueOf(2);
+
+	public static void main(String[] args) {
     	p = BigInteger.valueOf(101);
     	q = BigInteger.valueOf(103);
     	n = p.multiply(q);
@@ -25,5 +27,28 @@ public class main {
 		long elapsedTime = System.nanoTime() - startTime; 
 		System.out.println("ModPow-Time: " +elapsedTime);
 
+
+		/*BigInteger[] newArray = createArrayOfRandomBigInt(5, p);
+        System.out.println(newArray.length);
+        for (BigInteger value:newArray) {
+            System.out.println(value);
+        }*/
     }
+
+    static BigInteger[] createArrayOfRandomBigInt(int size, BigInteger maxValue) {
+        BigInteger[] bigIntegers = new BigInteger[size];
+        for (int i = 0; i < size; i++) {
+            bigIntegers[i] = randomBigint(maxValue);
+        }
+        return bigIntegers;
+    }
+
+	static BigInteger randomBigint(BigInteger max) {
+		Random randomGenerator = new Random();
+		BigInteger result;
+		do {
+			result = new BigInteger(max.bitLength(), randomGenerator);
+		} while (!(result.compareTo(TWO) >= 0 && result.compareTo(max) <= 0));
+		return result;
+	}
 }
